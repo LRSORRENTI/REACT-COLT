6d9183e0d9146e209dd5cb2b1849fb8cdb933f7c
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-11-15 09:51:12 -0600

    added more notes, and finished our first component section + added course files

--- our-first-component/index.js
+++ our-first-component/index.js
@@ -1,8 +1,14 @@
 class Hello extends React.Component {
     render() {
-        return <h1> Hello there! I am JSX! HTML inside of 
+       return (<h1> Hello there! I am JSX! HTML inside of 
             a JavaScript File
-        </h1>;
-    }
+        </h1>
+        
+        );
+     }
+ }
+
+function Dog(){
+    return <p> German Shepherd, Cava-Corgi </p>
 }
 ReactDOM.render(<Hello />, document.getElementById('root'));
