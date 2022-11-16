b073fdc2674ed9b8d60e233e0006248836f2a2c6
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-11-16 10:05:12 -0600

    finished section on React app structure, keeping compnents separate then rendering them in the main index.js App render

---
+++ App-structure/Hello.js
@@ -0,0 +1,12 @@
+class Hello extends React.Component {
+    render() {
+       return <h1> Hello there! I am JSX! HTML inside of 
+            a JavaScript File!!
+        </h1>
+        
+        
+     }
+ }
+
+
+
