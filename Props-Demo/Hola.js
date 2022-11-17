232dd999b882b34025a17001089e2ca9c1a01fc0
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-11-17 09:32:18 -0600

    added more on props, how to pass in different data-types, we need to use curly braces for everything except strings

--- Props-Demo/Hola.js
+++ Props-Demo/Hola.js
@@ -1,12 +1,17 @@
 class Hola extends React.Component {
     render() {
+        console.log(this.props)
         const props = this.props;
-       return <h1> Hola {this.props.to}, mi nombre es JSX, 
+        let bang = "!".repeat(props.bangs);
+       return (
+       <p>
+       <h1> Hola {this.props.to}, mi nombre es JSX, 
         y mi favorito amigo es HTML, from {this.props.from}, 
-        Adios {props.to}
+        Adios {props.to} {bang} 
         </h1>
-        
-        
+        <img src={this.props.img}/>
+        </p>
+       )
      }
  }
 
