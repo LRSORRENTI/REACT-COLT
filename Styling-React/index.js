940af3f101e460d30347b8d1190df025c920801f
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-11-21 09:18:56 -0600

    added section on styling React components, using inline styling and using variables to embed styling, and using an app.css file and linking to it for styling

---
+++ Styling-React/index.js
@@ -0,0 +1,29 @@
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Slot machine</h1>
+                <Machine 
+                s1="😊"
+                s2="😊"
+                s3="😊"
+    
+                />
+                  <Machine 
+                s1="😊"
+                s2="❤️"
+                s3="😊"
+    
+                />
+                  <Machine 
+                s1="😊"
+                s2="👌"
+                s3="😊"
+    
+                />
+                </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
